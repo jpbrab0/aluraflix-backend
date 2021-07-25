cbff88d248f48b075734c6ef2b2d0f22847af94d
Joao Pedro <jpresende502@gmail.com>
2021-07-25 13:25:19 -0300

    fix: removing the configuration for generate uuid in ecto

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,8 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 use Mix.Config
 
 config :aluraflix_backend,
-  ecto_repos: [AluraflixBackend.Repo],
-  generators: [binary_id: true]
+  ecto_repos: [AluraflixBackend.Repo]
 
-# Configures the endpoint
 config :aluraflix_backend, AluraflixBackendWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "ySaS8bFxhs6zETtbEQ60pZ5UMIfgNRlnefBfMumso5Mw/jI+HM7s40bIGCbaL89K",
@@ -19,14 +10,10 @@ config :aluraflix_backend, AluraflixBackendWeb.Endpoint,
   pubsub_server: AluraflixBackend.PubSub,
   live_view: [signing_salt: "hy0GF6+K"]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
