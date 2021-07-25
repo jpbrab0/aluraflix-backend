00d84c9df21c6a387da28be1efc39bcc06b830b1
Joao Pedro <jpresende502@gmail.com>
2021-07-25 19:21:02 -0300

    feat: creating the video view for return all the data

---
+++ lib/aluraflix_backend_web/views/video_view.ex
@@ -0,0 +1,29 @@
+defmodule AluraflixBackendWeb.VideoView do
+  use AluraflixBackendWeb, :view
+
+  def render("videos.json", %{videos: videos}) do
+    %{videos: render_many(videos, AluraflixBackendWeb.VideoView, "video.json")}
+  end
+
+  def render("video.json", %{video: video}) do
+    %{
+      video: %{
+        id: video.id,
+        titulo: video.titulo,
+        descricao: video.descricao,
+        url: video.url
+      }
+    }
+  end
+
+  def render("created_video.json", %{video: video}) do
+    %{
+      video: %{
+        id: video.id,
+        titulo: video.titulo,
+        descricao: video.descricao,
+        url: video.url
+      }
+    }
+  end
+end
