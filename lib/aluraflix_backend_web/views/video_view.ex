f993604f29ac65a572038780af93926e2ec4d21f
Joao Pedro <jpresende502@gmail.com>
2021-08-16 14:30:01 -0300

    feat: more views for the video controller

--- lib/aluraflix_backend_web/views/video_view.ex
+++ lib/aluraflix_backend_web/views/video_view.ex
@@ -7,12 +7,18 @@ defmodule AluraflixBackendWeb.VideoView do
 
   def render("video.json", %{video: video}) do
     %{
-      video: %{
-        id: video.id,
-        title: video.title,
-        description: video.description,
-        url: video.url
-      }
+      id: video.id,
+      title: video.title,
+      category_id: video.category_id,
+      description: video.description,
+      url: video.url
+    }
+  end
+
+  def render("videosbycategory.json", %{video_data: video_data}) do
+    %{
+      total: video_data.total_videos,
+      videos: render_many(video_data.videos, AluraflixBackendWeb.VideoView, "video.json")
     }
   end
 end
