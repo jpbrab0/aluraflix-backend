f44239ced497119ba95b67b0f6708d5a24a58078
Joao Pedro <jpresende502@gmail.com>
2021-08-19 18:08:22 -0300

    style: elixir linter

--- lib/aluraflix_backend/videos/schemas/video.ex
+++ lib/aluraflix_backend/videos/schemas/video.ex
@@ -23,6 +23,9 @@ defmodule AluraflixBackend.Videos.Schemas.Video do
     model
     |> cast(params, @required ++ @optional)
     |> validate_required(@required)
-    |> validate_format(:url, ~r/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/)
+    |> validate_format(
+      :url,
+      ~r/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/
+    )
   end
 end
