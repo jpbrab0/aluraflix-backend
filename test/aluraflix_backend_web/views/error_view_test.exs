60b3d51bd5173032d309cbed96f5bd48f135ba3b
Joao Pedro <jpresende502@gmail.com>
2021-08-19 18:09:46 -0300

    style: elixir linter

--- test/aluraflix_backend_web/views/error_view_test.exs
+++ test/aluraflix_backend_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule AluraflixBackendWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(AluraflixBackendWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(AluraflixBackendWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
